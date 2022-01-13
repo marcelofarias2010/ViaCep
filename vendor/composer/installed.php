81032dd657d316039bfd321ba291f149ecb4e085
Marcelo Farias <marcelofarias2010@users.noreply.github.com>
2022-01-12 21:14:18 -0300

    Busca inicial no ViaCep

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'marcelo/buscar-cep',
+  ),
+  'versions' => 
+  array (
+    'marcelo/buscar-cep' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
